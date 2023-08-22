a2ba38c169ea78f2ec5a8c7e13c4a715882cbeca
aleyna <aleynaebrt@gmail.com>
2023-08-22 14:19:45 +0300

    Nlog paketi eklendi ve loglama yapıldı.

--- BookApp/Program.cs
+++ BookApp/Program.cs
@@ -1,11 +1,14 @@
 
 using BookApp.Extensions;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 using Repositories.EfCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
+
+
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
@@ -18,6 +21,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
